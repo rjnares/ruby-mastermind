0b16ef3ddfae9ec70f15a9a0b283d35f8a183483
Roberto Nares <roberto.nares@gmail.com>
2024-07-17 16:36:06 -0700

    Create  basic game play loop skeleton which uses GameIO module to display relevant game text and terminates when @game_over flag is set to true

--- lib/mastermind_game.rb
+++ lib/mastermind_game.rb
@@ -2,11 +2,27 @@
 
 require_relative 'code_maker'
 require_relative 'code_breaker'
+require_relative 'game_io'
 
 # Creates a game of mastermind for two players: a code maker and a code breaker
 class MastermindGame
+  include GameIO
+
   def initialize
+    @game_over = false
     @code_maker = CodeMaker.new
     @code_breaker = CodeBreaker.new
+  end
+
+  def play
+    display_welcome_text
+    display_instructions_text
+
+    until @game_over
+      display_current_turn_text
+      @game_over = true
+    end
+
+    display_game_over_text
   end
 end
--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@
 
 require_relative 'lib/mastermind_game'
 
-MastermindGame.new
+MastermindGame.new.play
