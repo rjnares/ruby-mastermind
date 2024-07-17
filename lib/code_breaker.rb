d6dee74d6dc82f74aba46b2708f6c95c5b437989
Roberto Nares <roberto.nares@gmail.com>
2024-07-17 15:32:21 -0700

    Create the CodeBreaker class and create an instance of it in the MastermindGame class constructor

---
+++ lib/code_breaker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Creates a code breaker responsible for guessing the secret code every turn
+class CodeBreaker
+  def initialize
+    puts 'I am the code breaker!'
+  end
+end
--- lib/mastermind_game.rb
+++ lib/mastermind_game.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'code_maker'
+require_relative 'code_breaker'
 
 # Creates a game of mastermind for two players: a code maker and a code breaker
 class MastermindGame
   def initialize
     @code_maker = CodeMaker.new
+    @code_breaker = CodeBreaker.new
   end
 end
