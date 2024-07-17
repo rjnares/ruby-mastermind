58287fcf9ace9e5256877b2d5668378ec40a05ab
Roberto Nares <roberto.nares@gmail.com>
2024-07-17 15:38:50 -0700

    Create the Code class and create an instance of it in the CodeMaker class constructor

---
+++ lib/code.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Creates a mastermind game code consisting of four values
+class Code
+  def initialize
+    puts 'I am a code!'
+  end
+end
--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require_relative 'code'
+
 # Creates a code maker responsible for creating a secret code and giving feedback after every guess
 class CodeMaker
   def initialize
     puts 'I am the code maker!'
+    @secret_code = Code.new
   end
 end
