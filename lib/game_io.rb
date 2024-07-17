0ddcf3d2f59feb6ba330cc741855724b9291bd30
Roberto Nares <roberto.nares@gmail.com>
2024-07-17 16:32:41 -0700

    Create GameIO module containing game input/output utility methods

---
+++ lib/game_io.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Module containing game input/output utility methods
+module GameIO
+  def display_welcome_text
+    puts <<~TEXT
+
+      WELCOME_TEXT_PLACEHOLDER
+
+    TEXT
+  end
+
+  def display_instructions_text
+    puts <<~TEXT
+
+      INSTRUCTIONS_TEXT_PLACEHOLDER
+
+    TEXT
+  end
+
+  def display_current_turn_text
+    puts <<~TEXT
+
+      CURRENT_TURN_TEXT_PLACEHOLDER
+
+    TEXT
+  end
+
+  def display_game_over_text
+    puts <<~TEXT
+
+      GAME_OVER_TEXT_PLACEHOLDER
+
+    TEXT
+  end
+end
