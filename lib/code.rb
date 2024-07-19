971291e320861c25d3eac4b1dfd72319c7ca54eb
Roberto Nares <roberto.nares@gmail.com>
2024-07-18 20:01:07 -0700

    Refactor clue generation logic to fix bug where some guesses generate an exact and partial match for one number in the code

--- lib/code.rb
+++ lib/code.rb
@@ -7,19 +7,37 @@ class Code
   end
 
   def create_clues_from(other_code)
-    secret_code_num_frequency = @num_array.each_with_object(Hash.new(0)) { |digit, hash| hash[digit] += 1 }
-    other_code.num_array.each_with_object([]).each_with_index do |(digit, clues), index|
-      if digit == @num_array[index]
-        clues.prepend(true)
-        secret_code_num_frequency[digit] -= 1
-      elsif secret_code_num_frequency[digit].positive?
+    secret_code_num_frequency = create_secret_code_num_frequency_map
+    exact_match_clues = create_exact_match_clues(other_code, secret_code_num_frequency)
+    partial_match_clues = create_partial_match_clues(other_code, secret_code_num_frequency)
+    exact_match_clues + partial_match_clues
+  end
+
+  protected
+
+  attr_reader :num_array
+
+  private
+
+  def create_partial_match_clues(other_code, secret_code_num_frequency)
+    other_code.num_array.each_with_object([]).with_index do |(digit, clues), index|
+      if digit != @num_array[index] && secret_code_num_frequency[digit].positive?
         clues.push(false)
         secret_code_num_frequency[digit] -= 1
       end
     end
   end
 
-  protected
+  def create_exact_match_clues(other_code, secret_code_num_frequency)
+    other_code.num_array.each_with_object([]).with_index do |(digit, clues), index|
+      if digit == @num_array[index]
+        clues.push(true)
+        secret_code_num_frequency[digit] -= 1
+      end
+    end
+  end
 
-  attr_reader :num_array
+  def create_secret_code_num_frequency_map
+    @num_array.each_with_object(Hash.new(0)) { |digit, hash| hash[digit] += 1 }
+  end
 end
